40b40cddda3c135e3cbbb6fcc81e0d038d0f1362
Johannes Spies <13813209+neftlon@users.noreply.github.com>
2023-11-08 09:14:31 +0100

    make splits index based

--- src/load_data.py
+++ src/load_data.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from src.config import Config
 
 
-def get_data(config: Config):
+def get_data(config: Config, return_index: bool = False):
     # x has the format (num households H, observations N, input dim L)
     if config.mode.lower() == "prediction":
         data = np.load("data/task1_data.npz")
@@ -13,20 +13,35 @@ def get_data(config: Config):
         data = np.load("data/task2_data.npz")
     else:
         raise NotImplementedError("mode must be 'prediction' or 'classification'")
+
+    # move household dimension back for sampling
     x = data["x"].transpose(1, 0, 2)
     y = data["y"].transpose(1, 0)
     x, y = [torch.from_numpy(arr).float() for arr in [x, y]]
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=config.test_size, shuffle=config.shuffle,
-                                                        random_state=config.seed)
-    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=config.val_size,
-                                                      shuffle=config.shuffle, random_state=config.seed)
+    # generate train/validation/test split indices with desired ratio
+    full_ix = torch.arange(0, len(x))
+    trval_ix, te_ix = train_test_split(full_ix, test_size=config.test_size, 
+                                       shuffle=config.shuffle, 
+                                       random_state=config.seed)
+    tr_ix, val_ix = train_test_split(trval_ix, test_size=config.val_size,
+                                     shuffle=config.shuffle,
+                                     random_state=config.seed)
 
-    x_train, x_val, x_test = [arr.permute((1, 0, 2)) for arr in [x_train, x_val, x_test]]
-    y_train, y_val, y_test = [arr.permute((1, 0)) for arr in [y_train, y_val, y_test]]
+    # generate sets from indices
+    x_train, x_val, x_test = x[tr_ix], x[val_ix], x[te_ix]
+    y_train, y_val, y_test = y[tr_ix], y[val_ix], y[te_ix]
+
+    # move household dimension to front for consistency
+    x, x_train, x_val, x_test = [arr.permute((1, 0, 2)) for arr in [x, x_train, x_val, x_test]]
+    y, y_train, y_val, y_test = [arr.permute((1, 0)) for arr in [y, y_train, y_val, y_test]]
 
     # print([arr.shape for arr in [x_train, x_val, x_test, y_train, y_val, y_test]])
-    return x_train, x_val, x_test, y_train, y_val, y_test
+    splits = x_train, x_val, x_test, y_train, y_val, y_test
+    if return_index:
+        return splits, x, y, (tr_ix, val_ix, te_ix)
+    else:
+        return splits
 
 def load_daily_consumptions(
     path="./data/daily_consumptions.npz",
